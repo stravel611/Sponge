5075e8582d2e27ff6872d7714a21a541a9b1a8d0
stravel <1607535392@qq.com>
2019-01-17 13:49:45 +0800

    创建 API 蓝图和 Cli 命令。

--- app/__init__.py
+++ app/__init__.py
@@ -2,11 +2,23 @@
 from flask import Flask
 from app.settings import Config
 from app.exts import db
+from app.bp_api import api
+import click
+from app.models import Category, Item, Record, Tag, record_tag
 
 
 app = Flask(__name__)
 app.config.from_object(Config)
 db.init_app(app)
+
+
+app.register_blueprint(api)
+@app.cli.command()
+def init_db():
+    click.echo('正在初始化数据库...')
+    db.drop_all()
+    db.create_all()
+    click.echo('数据库初始化完毕！')
 
 
 if __name__ == '__main__':
