cac35a328981975e19b866d606e4b7f87d11494a
stravel <1607535392@qq.com>
2019-01-17 14:46:52 +0800

    添加测试用虚拟数据。

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,7 @@ from app.exts import db
 from app.bp_api import api
 import click
 from app.models import Category, Item, Record, Tag, record_tag
+import app.fakes as fake
 
 
 app = Flask(__name__)
@@ -13,12 +14,24 @@ db.init_app(app)
 
 
 app.register_blueprint(api)
+
+
 @app.cli.command()
 def init_db():
     click.echo('正在初始化数据库...')
     db.drop_all()
     db.create_all()
     click.echo('数据库初始化完毕！')
+
+
+@app.cli.command()
+def forge():
+    db.drop_all()
+    db.create_all()
+    fake.fake_categories()
+    fake.fake_items()
+    fake.fake_records()
+    fake.fake_tags()
 
 
 if __name__ == '__main__':
