971b3f5d2b56744d5d73d8fefdd73a36226464f3
stravel <1607535392@qq.com>
2019-01-17 13:48:51 +0800

    创建模型

--- app/models.py
+++ app/models.py
@@ -1,2 +1,45 @@
 # coding: utf-8
 from app.exts import db
+
+
+record_tag = db.Table(
+    'record_tag',
+    db.Column('record_id', db.Integer, db.ForeignKey('record.id'), primary_key=True),
+    db.Column('tag_id', db.Integer, db.ForeignKey('tag.id'), primary_key=True)
+)
+
+
+class Category(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(10))
+
+    items = db.relationship('Item', back_populates='category')
+
+
+class Item(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(10))
+
+    category_id = db.Column(db.Integer, db.ForeignKey('category.id'))
+
+    category = db.relationship('Category', back_populates='items')
+    records = db.relationship('Record', back_populates='item')
+
+
+class Record(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    start = db.Column(db.DateTime)
+    finish = db.Column(db.DateTime)
+    remark = db.Column(db.String(120))
+
+    item_id = db.Column(db.Integer, db.ForeignKey('item.id'))
+
+    item = db.relationship('Item', back_populates='records')
+    tags = db.relationship('Tag', secondary=record_tag, back_populates='records')
+
+
+class Tag(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(10))
+
+    records = db.relationship('Record', secondary=record_tag, back_populates='tags')
