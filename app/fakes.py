7176aa0e7770e6e442e7d5ff7206feef5fde7367
stravel <1607535392@qq.com>
2019-01-18 19:47:58 +0800

    将虚拟数据生成脚本添加到仓库。

---
+++ app/fakes.py
@@ -0,0 +1,61 @@
+# coding: utf-8
+from faker import Faker
+from app.models import Category, Item, Record, Tag
+from app.exts import db
+import random
+from sqlalchemy.exc import IntegrityError
+
+
+fake = Faker('zh_CN')
+
+
+def fake_categories(count=5):
+    for _ in range(count):
+        category = Category(name=fake.word())
+        db.session.add(category)
+    try:
+        db.session.commit()
+    except IntegrityError:
+        db.session.rollback()
+
+
+def fake_items(count=20):
+    categories = Category.query.all()
+    for _ in range(count):
+        item = Item(name=fake.word())
+        item.category = random.choice(categories)
+        db.session.add(item)
+    try:
+        db.session.commit()
+    except IntegrityError:
+        db.session.rollback()
+
+
+def fake_records(count=50):
+    items = Item.query.all()
+    times = [fake.date_time_this_year() for _ in range(count * 2)]
+    times.sort(reverse=True)
+    for _ in range(count):
+        record = Record(
+            start=times.pop(),
+            finish=times.pop(),
+            remark=fake.sentence()
+        )
+        record.item = random.choice(items)
+        db.session.add(record)
+    try:
+        db.session.commit()
+    except IntegrityError:
+        db.session.rollback()
+
+
+def fake_tags(count=10):
+    records = Record.query.all()
+    for _ in range(count):
+        tag = Tag(name=fake.word())
+        tag.records = random.sample(records, k=random.randint(0, len(records)))
+        db.session.add(tag)
+    try:
+        db.session.commit()
+    except IntegrityError:
+        db.session.rollback()
